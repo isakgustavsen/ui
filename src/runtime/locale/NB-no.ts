dac8d7b25d7b5a009f2ffb82648724ae37abb631
Isak Wang Gustavsen <69854945+isakgustavsen@users.noreply.github.com>
2024-11-22 13:56:54 +0100

    Create NB-no.ts

---
+++ src/runtime/locale/NB-no.ts
@@ -0,0 +1,45 @@
+export default defineLocale({
+  name: 'Norsk Bokmål',
+  code: 'NB-no',
+  messages: {
+    inputMenu: {
+      noMatch: '',
+      noData: 'Ingen data',
+      create: 'Opprett "{label}"'
+    },
+    inputNumber: {
+      increment: 'Øk',
+      decrement: 'Reduser'
+    },
+    commandPalette: {
+      noMatch: 'Ingen samsvarende data',
+      noData: 'Ingen data',
+      close: 'Lukk'
+    },
+    selectMenu: {
+      noMatch: 'Ingen samsvarende data',
+      noData: 'Ingen data',
+      create: 'Opprett "{label}"'
+    },
+    toast: {
+      close: 'Lukk'
+    },
+    carousel: {
+      prev: 'Forrige',
+      next: 'Neste',
+      goto: 'Gå til slide {slide}'
+    },
+    modal: {
+      close: 'Lukk'
+    },
+    slideover: {
+      close: 'Lukk'
+    },
+    alert: {
+      close: 'Lukk'
+    },
+    table: {
+      noData: 'Ingen data'
+    }
+  }
+})
